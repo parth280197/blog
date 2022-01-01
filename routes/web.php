f2e7979fe5b4e994b69a2ae72f72ccd3b404214c
Parth Patel <parth280197@gmail.com>
2022-01-01 17:57:56 -0600

    Section 4 -  Environment Files and Database Connections, Migrations: The Absolute Basics, Eloquent and the Active Record Pattern

--- routes/web.php
+++ routes/web.php
@@ -38,4 +38,35 @@ Route::get('/posts/{post}', function ($slug) {
  *
  * for forgetting cache
  * cache()->forget('posts.all')
+ *
+ * php artisan migrate - to create migration
+ * php artisan migrate:rollback - to rollback last migration
+ * php artisan migrate:fresh - to drop all table and create from scratch. (Should not be used in production)
+ *
+ * CREATING USER IN DB USING TINKER
+ *
+ * php artisan tinker
+ * Psy Shell v0.10.12 (PHP 8.0.12 — cli) by Justin Hileman
+ * >>> $user = new User;
+ * [!] Aliasing 'User' to 'App\Models\User' for this Tinker session.
+ * => App\Models\User {#3526}
+ * >>> $user->name = 'parth';
+ * => "parth"
+ * >>> $user->email = 'parth@email.com';
+ * => "parth@email.com"
+ * >>> $user->password = bcrypt('parth!');
+ * => "$2y$10$V6ilAIQohP1fW3pwMa5pfuB2N2hNvRRL33xYp2tuGqq.sA0P.z86q"
+ * >>> $user->save();
+ * => true
+ * >>> $user
+ * => App\Models\User {#3526
+     name: "parth",
+     email: "parth@email.com",
+     #password: "$2y$10$V6ilAIQohP1fW3pwMa5pfuB2N2hNvRRL33xYp2tuGqq.sA0P.z86q",
+     updated_at: "2022-01-01 23:51:55",
+     created_at: "2022-01-01 23:51:55",
+     id: 1,
+   }
+ *
+ *
  */
