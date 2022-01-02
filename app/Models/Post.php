9ed874ccaf5e74d50da9bf161c792a8fd1aa7642
Parth Patel <parth280197@gmail.com>
2022-01-01 18:59:55 -0600

    section 4 - 3 Ways to Mitigate Mass Assignment Vulnerabilities

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    //completely block mass assignment Post::create(['title'=>'title',......])
+    //completely block mass assignment - use if we are creating/updating data from forms.
+    protected $guarded = [];
+
+//    protected $guarded = ['id']; id is not mass assignable here
+
+//    protected $fillable = ['title', 'excerpt', 'body']; mentioned properties are mass assignable.
 }
